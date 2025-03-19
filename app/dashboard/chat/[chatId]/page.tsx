97166de21d333ae3414c7dd2e6537b27f4428cc7
anildegirmenci <anildegirmenci0671@gmail.com>
2025-03-19 15:58:04 +0300

    Chat and other components has been updated

--- app/dashboard/chat/[chatId]/page.tsx
+++ app/dashboard/chat/[chatId]/page.tsx
@@ -13,22 +13,26 @@ interface ChatPageProps {
     }>;
 }
 
-async function ChatPage({ params }: ChatPageProps){
-    const {chatId} = await params;
+async function ChatPage({ params }: ChatPageProps) {
+    const { chatId } = await params;
 
     const { userId } = await auth();
-    if (!userId){
+    if (!userId) {
         redirect("/");
     }
+    try {
+        const convex = getConvexClient();
+        const initialMessages = await convex.query(api.messages.list, { chatId });
 
-    const convex = getConvexClient();
+        return (
+            <div className="flex-1 overflow-hidden">
+                <ChatInterface chatId={chatId} initialMessages={initialMessages} />
+            </div>
+        );
+    } catch (error) {
+        console.error("Error loading chat");
+        redirect("/dashboard");
+    }
 
-    const initialMessages = await convex.query(api.messages.list, {chatId});
-
-
-
-    return <div>{chatId}</div>;
 }
-
-
 export default ChatPage;
--- components/ChatInterface.tsx
+++ components/ChatInterface.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client"
 
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useState } from "react";
+
+interface ChatInterfaceProps {
+  chatId: Id<"chats">;
+  initialMessages: Doc<"messages">[];
+}
 function ChatInterface() {
+  const [messages, setMessages] = useState<Doc<"messages">[]>(initialMessages);
+  const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+
   return (
     <div>
-      
+      Hello!!
     </div>
   )
 }
